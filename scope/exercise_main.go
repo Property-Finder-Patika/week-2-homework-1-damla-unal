5d217449f0b928302121d7129a7e478fd6024f50
damla-unal <damlaunal.cs@gmail.com>
2022-07-01 10:17:53 +0300

    week2: add example about scope from inancgumus learngo

--- scope/exercise_main.go
+++ scope/exercise_main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// run with this command : go run exercise_main.go exercise_printer.go
+// If you just run go run exercise_main.go and that file has a reference to a function in another file within the same package,
+// it will error because you didn't tell Go to run the whole package, you told it to only run that one file.
+
+//You can tell go to run as a whole package by grouping the files as a package in the run command in several ways.
+// like go run ./ or go run exercise_main.go exercise_printer.go
 func main() {
 	hello() //no need to import package because these are in same package
 }
