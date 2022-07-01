2e9eb025d4042d01edd1aa0afd44b9da9369532f
damla-unal <damlaunal.cs@gmail.com>
2022-07-01 12:54:42 +0300

    week2: add example about scope from week2 class

---
+++ statement_expressions/excercise.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Uncomment the code line below to see the error.
+
+	/*
+		"Hello"
+	*/
+
+	// It will say: "evaluated but not used"
+	//
+	// Because:
+	// "Hello" literal returns a value but there isn't any
+	// statement which uses it.
+	//
+	// So:
+	// You can't use expressions alone without statements.
+
+	fmt.Println("*************************")
+	// Operators combine multiple expressions together
+	// as if there's a single expression.
+	fmt.Println("Hello!" + "!" + "!" + "?")
+}
